2d7841f8940a8d3a05d00387ba6fb2c7c33630a4
Kritik Modi <kritik.modi@juspay.in>
2023-07-31 17:45:31 +0530

    Update code structure

--- server.js
+++ server.js
@@ -1,28 +1,34 @@
 const express = require("express");
 const app = express();
-// This is a public sample test API key.
-// Don’t submit any personally identifiable information in requests made with this key.
-// Sign in to see your own test API key embedded in code samples.
+
+const hyperswitch = require("@juspay-tech/hyperswitch-node")('HYPERSWITCH_API_KEY');
+
 app.use(express.static("public"));
 app.use(express.json());
 
+const calculateOrderAmount = (items) => {
+  return 1345;
+};
+
 app.post("/create-payment", async (req, res) => {
 
-    fetch("https://sandbox.hyperswitch.io/payments", {
-        method: "POST",
-        headers: { "Content-Type": "application/json", 'api-key': "API_KEY" },
-        body: JSON.stringify({
-            currency: "USD",
-            amount: 100,
-            customer_id: "juspay",
-        }),
-    })
-        .then(resp => resp.json())
-        .then(data => {
-            res.send({
-                clientSecret: data.client_secret
-            })
-        })
-})
+  const { items } = req.body;
+
+  /*
+    If you have two or more “business_country” + “business_label” pairs configured in your Hyperswitch dashboard,
+    please pass the fields business_country and business_label in this request body.
+    For accessing more features, you can check out the request body schema for payments-create API here :
+    https://api-reference.hyperswitch.io/docs/hyperswitch-api-reference/60bae82472db8-payments-create
+  */
+
+  const paymentIntent = await hyperswitch.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "USD",
+  });
+
+  res.send({
+    client_secret: paymentIntent.client_secret,
+  });
+});
 
 app.listen(4242, () => console.log("Node server listening on port 4242!"));
